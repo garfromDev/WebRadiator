2db306c42642097ea8439a51370834ba46d0f096
garfromDev <garfromdev@gmail.com>
2022-08-17 22:19:37 +0200

    ajout config pycharm flask + templates et formulaire

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Nom", validators=[DataRequired()])
+    password = PasswordField("Mor de passe", validators=[DataRequired()])
+    remember_me = BooleanField("Se souvenir de moi")
+    submit = SubmitField("S'identifier")
