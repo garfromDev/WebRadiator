66851d1967045df82b3954dc3b282dd3a2987292
garfromDev <garfromdev@gmail.com>
2022-08-17 19:16:12 +0200

    ajout template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,30 @@
 from app import app
+from flask import render_template
+import random
+
+class Radiator:
+    @property
+    def temperature(self):
+        return random.randint(15,25)
+
+    @property
+    def connected(self):
+        return bool(random.choice([True, False]))
+
+
+posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
+
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def main_page():
+    radiator = Radiator()
+    return render_template('index.html', title='Radiator', radiator=radiator, posts=posts)
