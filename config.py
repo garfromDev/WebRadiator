9d068a393de7acf61424534153521282a7f2e71a
garfromDev <garfromdev@gmail.com>
2022-09-12 22:31:12 +0200

    INtégration du projet Radiator et accès à la température via InsideCondition Les variales d'environnement ne marchent pas

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "nimportequoi"
+    #RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT') or False
