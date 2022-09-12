938ead4a96983784b4db3da2951c1fa809db993c
garfromDev <garfromdev@gmail.com>
2022-09-12 22:31:12 +0200

    INtégration du projet Radiator et accès à la température via InsideCondition Les variales d'environnement ne marchent pas

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from config import Config
+import os
+import sys
+# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
+# at this point, ENV doesn't contain ENV variables declared in Pycharm settings
 
 app = Flask(__name__)
 app.config.from_object(Config)
+#print("secret :", app.config['RADIATOR_TEST_ENVIRONMENT'])
 print("secret :", app.config['SECRET_KEY'])
+os.environ["RADIATOR_TEST_ENVIRONMENT"] = "TRUE"
+sys.path.append('/Users/alistef/Documents/Programmation/Rasberry/Radiator/')
+from  main import start_radiator
+print("staring radiator")
+os.chdir('/Users/alistef/Documents/Programmation/Rasberry/Radiator/')
+start_radiator()
 
 from app import routes
