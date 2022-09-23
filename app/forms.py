0972c811d283174ee1993d4ec0904b8a6dec8edd
garfromDev <garfromdev@gmail.com>
2022-09-23 22:42:16 +0200

    AJout de validator, de message d'erreurs, de messages flash, ajout de alchemy en dépendance pour préparer db à la place du json Les variales d'environnement ne marchent pas

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, NoneOf
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Nom", validators=[DataRequired()])
-    password = PasswordField("Mor de passe", validators=[DataRequired()])
+    username = StringField("Nom", validators=[DataRequired(), NoneOf(['toto', 'admin'])])
+    password = PasswordField("Mor de passe", validators=[DataRequired(), Length(min=5)])
     remember_me = BooleanField("Se souvenir de moi")
     submit = SubmitField("S'identifier")
 
