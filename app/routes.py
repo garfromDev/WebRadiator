1a015365a330f152cf4676b59c39171b7416dfa0
garfromDev <garfromdev@gmail.com>
2022-09-23 22:42:16 +0200

    AJout de validator, de message d'erreurs, de messages flash, ajout de alchemy en dépendance pour préparer db à la place du json Les variales d'environnement ne marchent pas

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
 import random
 
 from app.forms import LoginForm
@@ -32,8 +32,13 @@ def main_page():
     radiator = InsideCondition.shared()
     return render_template('index.html', title='Radiator', radiator=radiator, posts=posts)
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
 
-    return render_template("login.html", title="Login", form=form)
+@app.route("/login", methods=["GET", "POST"])
+def login_post():
+    # TODO login
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for("index"))
+    return render_template('login.html', title='Sign In', form=form)
