3e94195d4f9bd5e2687f8144bb84c19ef7923bf9
garfromDev <garfromdev@gmail.com>
2022-09-24 19:38:51 +0200

    fix des variable environement pour mode test radiator ajout en cours de la base sqlAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 import sys
 # the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
@@ -7,13 +9,17 @@ import sys
 
 app = Flask(__name__)
 app.config.from_object(Config)
-#print("secret :", app.config['RADIATOR_TEST_ENVIRONMENT'])
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # pour le suivi des migrations de la base
+
+print("test :", app.config['RADIATOR_TEST_ENVIRONMENT'])
 print("secret :", app.config['SECRET_KEY'])
-os.environ["RADIATOR_TEST_ENVIRONMENT"] = "TRUE"
+os.environ["RADIATOR_TEST_ENVIRONMENT"] = str(app.config['RADIATOR_TEST_ENVIRONMENT'])
+print("test", os.environ.get("RADIATOR_TEST_ENVIRONMENT"))
 sys.path.append('/Users/alistef/Documents/Programmation/Rasberry/Radiator/')
 from  main import start_radiator
 print("staring radiator")
 os.chdir('/Users/alistef/Documents/Programmation/Rasberry/Radiator/')
 start_radiator()
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "nimportequoi"
-    #RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT') or False
+    RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT', True)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(base_dir, "app.db"))
+    SQLALCHEMY_TRACK_MODIFICATION = False  # evite d'envoyer un signal à chaque modif de la base
