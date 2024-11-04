0e90e96b2d9989b078a48beda1d3ea995adf3e00
garfromDev <garfromdev@gmail.com>
2024-11-04 20:20:32 +0100

    utilisation de FlaskSCheduler permet la mémorisation en DB des décisions prises

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import os
+import logging
 import sys
 # the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
 # at this point, ENV doesn't contain ENV variables declared in Pycharm settings
 
 app = Flask(__name__)
+logger = logging.getLogger('werkzeug') # grabs underlying WSGI logger
+handler = logging.FileHandler('test.log') # creates handler for the log file
+logger.addHandler(handler) #
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'  # so flask know how to log users
@@ -23,6 +27,7 @@ os.environ["RADIATOR_TEST_ENVIRONMENT"] = str(app.config['RADIATOR_TEST_ENVIRONM
 print("test", os.environ.get("RADIATOR_TEST_ENVIRONMENT"))
 from Radiator.main import start_radiator
 print("starting radiator")
-start_radiator(app, avoid_flash=True)
+with app.app_context():
+    start_radiator(app, avoid_flash=True)
 
 from app import routes, models
