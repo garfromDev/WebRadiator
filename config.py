66efd5f44ec1e26156388b7b271080c92eb91130
garfromDev <garfromdev@gmail.com>
2024-12-08 16:11:47 +0100

    passage de l'app à UserInteractionManager pour bénéficier du context

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "nimportequoi"
-    RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT', True)
+    RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT', False)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(base_dir, "app.db"))
     print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATION = False  # evite d'envoyer un signal à chaque modif de la base
