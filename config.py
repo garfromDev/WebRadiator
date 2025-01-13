8af29842fe318869564dfc2368525bf2ae80f483
garfromDev <garfromdev@gmail.com>
2025-01-13 22:12:15 +0100

    :recycle: ne plus instancier le DecisionMaker a chaque cycle, et ne plus utiliser le séquenceur "maison" pour éviter les plantages

--- config.py
+++ config.py
@@ -6,5 +6,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "nimportequoi"
     RADIATOR_TEST_ENVIRONMENT = os.environ.get('RADIATOR_TEST_ENVIRONMENT', "False")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(base_dir, "app.db"))
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATION = False  # evite d'envoyer un signal à chaque modif de la base
