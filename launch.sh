cd8444ca693c29e6e7f1a3294d07323d0f8f5598
garfromDev <garfromdev@gmail.com>
2025-01-15 19:41:29 +0100

    :wrench: changement de venv pour le launch

--- launch.sh
+++ launch.sh
@@ -2,5 +2,5 @@
 # allow to launch from crontab using  @reboot sleep 60 && bash  /home/pi/WebRadiator/launch.sh
 # may also be used from terminal to launch  the application
 cd /home/pi/WebRadiator
-source .venv/webRadiator/bin/activate
+source webRadiator/bin/activate
 export RADIATOR_TEST_ENVIRONMENT="false";flask run --host=0.0.0.0 -p 3000
